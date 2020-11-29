d4950cd0462028e1a33e669be8c917eefeb3af64
Andrii Siriak <siryaka@gmail.com>
2020-11-29 09:49:11 +0200

    Add setup for iot monitor app

---
+++ IotMonitor/IotSupervisor.cs
@@ -0,0 +1,20 @@
+using Akka.Actor;
+using Akka.Event;
+
+namespace IotMonitor
+{
+    public class IotSupervisor : UntypedActor
+    {
+        public ILoggingAdapter Log { get; } = Context.GetLogger();
+
+        protected override void PreStart() => Log.Info("IoT Application started");
+        protected override void PostStop() => Log.Info("IoT Application stopped");
+
+        // No need to handle any messages
+        protected override void OnReceive(object message)
+        {
+        }
+
+        public static Props Props() => Akka.Actor.Props.Create<IotSupervisor>();
+    } 
+}
---
+++ IotMonitor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Akka.Actor;
+
+namespace IotMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class IotApp
+    {
+        public static void Init()
+        {
+            using var system = ActorSystem.Create("iot-system");
+            // Create top level supervisor
+            var supervisor = system.ActorOf(IotSupervisor.Props(), "iot-supervisor");
+            // Exit the system after ENTER is pressed
+            Console.ReadLine();
+        }
+    }
+}
